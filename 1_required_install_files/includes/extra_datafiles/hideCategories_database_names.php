51672b41a9d5cdc20d3ab830d74b5d2214426369
Jim King <jeking@wheatonwebsiteservices.com>
2015-02-10 20:20:12 -0600

    Initial Commit
    Version 1.3.9

---
+++ 1_required_install_files/includes/extra_datafiles/hideCategories_database_names.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * hideCategories
+ * @copyright Copyright 2006 s_mack
+ * @copyright Portions Copyright 2003-2006 Zen Cart Development Team
+ */
+  define('TABLE_HIDE_CATEGORIES', DB_PREFIX . 'hide_categories');
+?>
