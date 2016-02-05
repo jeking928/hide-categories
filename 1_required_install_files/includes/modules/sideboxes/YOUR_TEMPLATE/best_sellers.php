4bb12062e308355a5a5793e72e4cf5c6f09bbb48
James King <jeking@wheatonwebsiteservices.com>
2016-02-05 11:24:37 -0600

    Initial update for 1.5.5
    Update core files to Zen Cart 1.5.5

--- 1_required_install_files/includes/modules/sideboxes/YOUR_TEMPLATE/best_sellers.php
+++ 1_required_install_files/includes/modules/sideboxes/YOUR_TEMPLATE/best_sellers.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * best_sellers sidebox - displays selected number of (usu top ten) best selling products
+ * best_sellers sidebox - displays selected number of (usually top ten) best selling products
  *
  * @package templateSystem
- * @copyright Copyright 2003-2011 Zen Cart Development Team
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: best_sellers.php 18941 2011-06-13 22:12:42Z wilt $
+ * @version $Id: best_sellers.php DrByte  Modified in v1.5.5 $
  */
 
 // test if box should display
@@ -32,7 +32,7 @@
   if ($show_best_sellers == true) {
     $limit = (trim(MAX_DISPLAY_BESTSELLERS) == "") ? "" : " LIMIT " . (int)MAX_DISPLAY_BESTSELLERS;
   	if (isset($current_category_id) && ($current_category_id > 0)) {
-      $best_sellers_query = "select distinct p.products_id, pd.products_name, p.products_ordered
+      $best_sellers_query = "select distinct p.products_id, pd.*, p.*
                              from " . TABLE_HIDE_CATEGORIES . " h, " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd, "
                                     . TABLE_PRODUCTS_TO_CATEGORIES . " p2c, " . TABLE_CATEGORIES . " c
                              where p.products_status = '1'
@@ -42,7 +42,7 @@
                              and p.products_id = p2c.products_id
                              and p2c.categories_id = c.categories_id
                              and '" . (int)$current_category_id . "' in (c.categories_id, c.parent_id)
-							 and (p.master_categories_id = h.categories_id and h.visibility_status !=2) 
+		             and (p.master_categories_id = h.categories_id and h.visibility_status !=2) 
                              order by p.products_ordered desc, pd.products_name";
 
       $best_sellers_query .= $limit;
@@ -52,13 +52,13 @@
 //  End hideCategories code*/
       $best_sellers = $db->Execute($best_sellers_query);
     } else {
-      $best_sellers_query = "select distinct p.products_id, pd.products_name, p.products_ordered
+      $best_sellers_query = "select distinct p.products_id, pd.*, p.*
                              from " . TABLE_HIDE_CATEGORIES . " h, " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd
                              where p.products_status = '1'
                              and p.products_ordered > 0
                              and p.products_id = pd.products_id
                              and pd.language_id = '" . (int)$_SESSION['languages_id'] . "'
-							 and (p.master_categories_id = h.categories_id and h.visibility_status !=2)
+			     and (p.master_categories_id = h.categories_id and h.visibility_status !=2)
                              order by p.products_ordered desc, pd.products_name";
 
       $best_sellers_query .= $limit;
@@ -75,6 +75,11 @@ if ($best_sellers->RecordCount() >= MIN_DISPLAY_BESTSELLERS) {
         $rows++;
         $bestsellers_list[$rows]['id'] = $best_sellers->fields['products_id'];
         $bestsellers_list[$rows]['name']  = $best_sellers->fields['products_name'];
+        $bestsellers_list[$rows]['image'] = zen_image(DIR_WS_IMAGES . $best_sellers->fields['products_image'], $best_sellers->fields['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT);;
+        $bestsellers_list[$rows]['href'] = zen_href_link(zen_get_info_page($best_sellers->fields["products_id"]), 'cPath=' . zen_get_generated_category_path_rev($best_sellers->fields["master_categories_id"]) . '&products_id=' . $best_sellers->fields["products_id"]);
+        $bestsellers_list[$rows]['price'] = zen_get_products_display_price((int)$best_sellers->fields['products_id']);
+        $bestsellers_list[$rows]['model']  = $best_sellers->fields['products_model'];
+        $bestsellers_list[$rows]['description']  = $best_sellers->fields['products_description'];
         $best_sellers->MoveNext();
       }
 
@@ -84,4 +89,3 @@ if ($best_sellers->RecordCount() >= MIN_DISPLAY_BESTSELLERS) {
       require($template->get_template_dir($column_box_default, DIR_WS_TEMPLATE, $current_page_base,'common') . '/' . $column_box_default);
     }
   }
-?>
